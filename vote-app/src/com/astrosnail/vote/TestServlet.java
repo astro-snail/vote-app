a632838047dc607bab86b46bbc0df200f81ff59e
Elena Ilyukhina <elena.ilyukhina@hotmail.com>
2017-03-13 02:12:48 +0100

    Changed TestServlet to output some messages

--- vote-app/src/com/astrosnail/vote/TestServlet.java
+++ vote-app/src/com/astrosnail/vote/TestServlet.java
@@ -3,9 +3,8 @@ package com.astrosnail.vote;
 import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.Statement;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -31,41 +30,55 @@ public class TestServlet extends HttpServlet {
 		PrintWriter out = response.getWriter();
 		response.setContentType("text/plain");
 		
-		Connection connection = null;
-		Statement statement = null;
-		ResultSet result = null;
+		List<Candidate> candidates = new ArrayList<>();
+		
+		boolean isError = false;
 		
 		try {
 			System.setProperty("derby.system.home", getServletContext().getRealPath(File.separator));
 			
-			/*connection = dataSource.getConnection();
-			String sql = "select * from candidates";
-			statement = connection.createStatement();
-			
-			result = statement.executeQuery(sql);
-			
-			while (result.next()) {
-				String candidateName = result.getString("candidate_name");
-				out.println(candidateName);
-			}*/
-			
+			/* Test database connection */
 			VoteDbUtil voteDbUtil = new VoteDbUtil(dataSource);
-
-			List<Candidate> candidates = voteDbUtil.getCandidates();
+			try {
+				candidates = voteDbUtil.getCandidates();
+			} catch (SQLException e) {
+				voteDbUtil.setUpDatabase();
+				candidates = voteDbUtil.getCandidates();
+			}
 			
-			/*for (int i = 0; i < 1000000; i++) {
-				for (int j = 0; j < candidates.size(); j++) {
-					Vote vote = new Vote(new User(i), new Candidate((short)(j+1)));
-					voteDbUtil.addVote(vote);
-				}	
-			}*/
+			if (candidates.size() != 6) {
+				isError = true;
+				out.println("Number of candidates is " + candidates.size());
+		    }
 			
+			/* Test votes are being added */
 			Map<Short, Long> voteResult = voteDbUtil.getVoteResult();
 			
-			for (Short key : voteResult.keySet()) {
-				out.println(key + " " + voteResult.get(key));
+			long id = 999999999L;
+			User user = voteDbUtil.getUser(id);
+			Candidate candidate = candidates.get(0);
+	
+			long totalVotes = 0;
+			if (voteResult.containsKey(candidate.getId())) {
+				totalVotes = voteResult.get(candidate.getId());
 			}
+			
+			/* Add vote */
+			voteDbUtil.addVote(new Vote(user, candidate));
+						
+			/* Add vote */
+			voteDbUtil.addVote(new Vote(user, candidate));
 				
+			voteResult = voteDbUtil.getVoteResult();
+			if (voteResult.get(candidate.getId()) - totalVotes != 2) {
+				isError = true;
+				out.println("Number of votes added is wrong");
+			}
+			
+			if (!isError) {
+				out.println("No errors");
+			}
+										
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
